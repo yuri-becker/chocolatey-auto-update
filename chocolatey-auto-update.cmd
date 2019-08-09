75a655519d7745952865f5730bb5767b3f5d3138
Yuri Becker <hi@yuri.li>
2019-08-09 23:42:53 +0200

    Add install script

---
+++ chocolatey-auto-update.cmd
@@ -0,0 +1,1 @@
+powershell -f chocolatey-auto-update.ps1
---
+++ install.ps1
@@ -0,0 +1,7 @@
+﻿$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\chocolatey-auto-update.lnk")
+$Shortcut.TargetPath = "start"
+$Shortcut.Arguments = "/min cmd /c $((Split-Path -parent $PSCommandPath))\chocolatey-auto-update.cmd"
+$Shortcut.WorkingDirectory = $(Split-Path -parent $PSCommandPath)
+$Shortcut.WindowStyle = 7
+$Shortcut.Save()
